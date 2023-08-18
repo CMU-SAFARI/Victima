f78985dd8b21f4eac5110482b8fa647686e1c269
Konstantinos Kanellopoulos <konkanello@gmail.com>
2023-08-18 17:05:58 +0300

    Added documentation

---
+++ documentation/script.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    alert("Touched button");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
