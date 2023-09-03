a6ae715ad40668998ab421c05fa40031cb2445c8
kanellok <kanellok@safari-sim0.ee.ethz.ch>
2023-09-03 21:34:14 +0200

    Fixed pulling images

--- scripts/launch_jobs.py
+++ scripts/launch_jobs.py
@@ -48,7 +48,7 @@ traces = [("bc", "bc.sift"),
 
 # Docker command to run the binary inside the container
 docker_command = "docker run --rm -v "+args.path + \
-    ":/app/ kanell21/artifact_evaluation:victima"
+    ":/app/ docker.io/kanell21/artifact_evaluation:victima"
 
 baseline = " -c /app/sniper/config/virtual_memory_configs/radix.cfg "
 victima = " -c /app/sniper/config/virtual_memory_configs/victima.cfg "
