1ddeacca230b771d2e879fe1db3a43595e4915d8
kanellok <kanellok@safari-sim0.ee.ethz.ch>
2023-09-08 23:03:01 +0200

    Minor fixes in plotting

--- scripts/create_plots.py
+++ scripts/create_plots.py
@@ -147,10 +147,17 @@ pivot_table_fig2 = pivot_table_fig2[selected_experiments]
 ax = pivot_table_fig2.plot(kind='bar', figsize=(12, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
 
+# Modify this list as per your requirements
+new_legend_labels = ['L2 TLB 1.5K entries',
+                     '2K', '4K', '8K', '16K', '32K', '64K']
+# ax.legend(new_legend_labels, title='Configuration',
+#           bbox_to_anchor=(1.05, 1), loc='upper left')
+
 plt.xlabel('Trace')
 plt.ylabel('L2 TLB MPKI')
 plt.title('Figure 2: L2 TLB MPKI for different TLB sizes')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 
 # Replace 'output_path.png' with the desired file path and name for the saved image
@@ -186,11 +193,13 @@ print(pivot_table_fig3)
 
 ax = pivot_table_fig3.plot(kind='bar', figsize=(12, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['L2 TLB 2K entries 12-cycle latency',
+                     '4K-12', '8K-12', '16K-12', '32K-12', '64K-12']
 plt.xlabel('Trace')
 plt.ylabel('Normalized Speedup')
 plt.title('Figure 3: Normalized Speedup for different L2 TLB sizes')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.ylim(0.9, 1.15)
 
 plt.tight_layout()
@@ -238,10 +247,14 @@ print(pivot_table_fig4)
 ax = pivot_table_fig4.plot(kind='bar', figsize=(12, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
 
+new_legend_labels = ['L3 TLB 64K entries 15-cycle latency',
+                     '64K-20', '64K-25', '64K-30', '64K-35', '64K-40']
+
 plt.xlabel('Trace')
 plt.ylabel('Normalized Speedup')
 plt.title('Figure 4: Normalized Speedup for different L3 TLB sizes')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.ylim(0.9, 1.15)
 
 plt.tight_layout()
@@ -283,13 +296,17 @@ print(pivot_table_fig6)
 ax = pivot_table_fig6.plot(kind='bar', stacked=True, figsize=(15, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
 
+new_legend_labels = ['Reuse 0',
+                     '1-5', '5-10', '10-20', '>20']
+
+
 plt.title('Figure 6: Reuse of L2 cache blocks')
 plt.xlabel('Traces')
 plt.ylabel('Breakdown of reuse (%)')
+plt.legend(new_legend_labels, title="Reuse", bbox_to_anchor=(1.05, 1.0))
 
 plt.tight_layout()
 plt.ylim(80, 100)
-plt.legend(title="Reuse", bbox_to_anchor=(1.5, 1.0))
 # Replace 'output_path.png' with the desired file path and name for the saved image
 plt.savefig('./plots/figure6.png')
 
@@ -325,11 +342,14 @@ pivot_table_fig15.drop('tlb_base_ideal', axis=1, inplace=True)
 ax = pivot_table_fig15.plot(kind='bar', figsize=(12, 3))
 
 ax.set_xticklabels(desired_order_labels_gmean)
+new_legend_labels = ['POM-TLB 64K',
+                     'Opt. L3 TLB 64K', 'Opt. L2 TLB 64K', 'Opt. L2 TLB 128K', 'Victima']
 
 plt.xlabel('Trace')
 plt.ylabel('Normalized Speedup')
 plt.title('Figure 15: Normalized Speedup')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.ylim(0.9, 1.3)
 
 plt.tight_layout()
@@ -371,11 +391,13 @@ pivot_table_fig16_subbed.drop('tlb_base_ideal', axis=1, inplace=True)
 ax = pivot_table_fig16_subbed.plot(kind='bar', figsize=(12, 3))
 
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['POM-TLB 64K',
+                     'Opt. L2 TLB 64K', 'Opt. L2 TLB 128K', 'Victima']
 plt.xlabel('Trace')
 plt.ylabel('PTW Reduction (%)')
 plt.title('Figure 16: Reduction of PTWs across different configurations')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 
 plt.tight_layout()
 plt.savefig('./plots/figure16.png')
@@ -409,15 +431,17 @@ pivot_table_fig19.loc['GMEAN'] = pivot_table_fig19.mean()
 print(pivot_table_fig19)
 
 
-ax = pivot_table_fig19.plot(kind='bar', stacked=True, figsize=(15, 3))
+ax = pivot_table_fig19.plot(kind='bar', stacked=True, figsize=(12, 6))
 
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['Reuse 0',
+                     '1-5', '5-10', '10-20', '>20']
 
 plt.title('Figure 19: L2 Cache TLB Reuse with Victima')
 plt.xlabel('Traces')
 plt.ylabel('Breakdown of L2 TLB Reuse (%)')
-plt.legend(loc='upper right', title='Reuse', bbox_to_anchor=(1.55, 1))
+plt.legend(new_legend_labels, loc='upper right',
+           title='Reuse', bbox_to_anchor=(1.1, 1))
 
 plt.tight_layout()
 plt.ylim(0, 100)
@@ -474,11 +498,12 @@ print(pivot_table_fig20)
 ax = pivot_table_fig20.plot(kind='bar', figsize=(12, 3))
 
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['1MB', '2MB', '4MB', '8MB']
 plt.xlabel('Trace')
 plt.ylabel('Reduction of PTWs (%)')
 plt.title('Figure 20: Reduction of PTWs across different L2 Cache Sizes')
-plt.legend(title='Configuration', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configuration',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 
 plt.tight_layout()
 plt.savefig('./plots/figure20.png')
@@ -514,12 +539,14 @@ pivot_table_fig22.drop('baseline_radix_virtualized', axis=1, inplace=True)
 print(pivot_table_fig22)
 ax = pivot_table_fig22.plot(kind='bar', figsize=(12, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['POM-TLB',
+                     'Ideal Shadow Paging', 'Victima']
 plt.xlabel('Trace')
 plt.ylabel('Normalized Speedup')
 plt.title('Figure 22: Normalized Speedup for different configurations')
-plt.legend(title='Experiment', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.ylim(0.9, 1.3)
+plt.legend(new_legend_labels, title='Experiment',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.ylim(0.9, 1.5)
 
 plt.tight_layout()
 plt.savefig('./plots/figure22.png')
@@ -594,11 +621,13 @@ print(pivot_table_fig23)
 
 ax = pivot_table_fig23.plot(kind='bar', figsize=(12, 3))
 ax.set_xticklabels(desired_order_labels_gmean)
-
+new_legend_labels = ['POM-TLB Guest PTW',
+                     'POM-TLB Host PTW', 'Victima Guest PTW', 'Victima Host PTW']
 plt.xlabel('Trace')
 plt.ylabel('PTW Reduction (%)')
 plt.title('Figure 23: Host and Guest PTW Reduction in virtualized environments')
-plt.legend(title='Configurations', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(new_legend_labels, title='Configurations',
+           bbox_to_anchor=(1.05, 1), loc='upper left')
 
 plt.tight_layout()
 plt.savefig('./plots/figure23.png')
@@ -636,45 +665,82 @@ final_df = reset_df.set_index(["Trace", "Exp"])
 
 plot_data = final_df.pivot_table(index=["Trace", "Exp"], values=[
                                  'guest_ptw_latency', 'host_ptw_latency'])
-
 print(plot_data)
+
+# Create a mapping of old names to new names
+name_mapping = {
+    'baseline_radix_virtualized': 'Baseline',
+    'tlb_base_ideal': 'I-SP',
+    'potm_virtualized': 'POM-TLB',
+    'victima_virtualized': 'Victima'
+}
+
+# Rename the values in the 'Exp' level
+plot_data.index = plot_data.index.set_levels(
+    [[name_mapping.get(exp, exp) for exp in plot_data.index.levels[1]]], level=[1]
+)
+plot_data = plot_data.drop('Baseline', level='Exp')
 
 
 fig, ax = plt.subplots(figsize=(12, 8))
 
 # For proper labeling and positioning
-labels = []
-positions = []
+# labels = []
+# positions = []
 
-# Track the width for stacked bars
-width = 0.35
+# # Track the width for stacked bars
+# width = 0.35
 
-for i, (trace, sub_df) in enumerate(plot_data.groupby(level=0)):
-    # Extract positions for custom labels
-    pos = range(i*len(sub_df), (i+1)*len(sub_df))
-    positions.extend(pos)
+# for i, (trace, sub_df) in enumerate(plot_data.groupby(level=0)):
+#     # Extract positions for custom labels
+#     pos = range(i*len(sub_df), (i+1)*len(sub_df))
+#     positions.extend(pos)
 
-    # Extract labels
-    labels.extend(sub_df.index.get_level_values(1))
+#     # Extract labels
+#     labels.extend(sub_df.index.get_level_values(1))
 
-    # Plot each 'Exp' for the given 'Trace'
-    if i == 0:
-        ax.bar(pos, sub_df['guest_ptw_latency'],
-               width, label='guest_ptw_latency')
-        ax.bar(pos, sub_df['host_ptw_latency'], width,
-               bottom=sub_df['guest_ptw_latency'], label='host_ptw_latency')
-    else:
-        ax.bar(pos, sub_df['guest_ptw_latency'], width)
-        ax.bar(pos, sub_df['host_ptw_latency'], width,
-               bottom=sub_df['guest_ptw_latency'])
+#     # Plot each 'Exp' for the given 'Trace'
+#     if i == 0:
+#         ax.bar(pos, sub_df['guest_ptw_latency'],
+#                width, label='guest_ptw_latency')
+#         ax.bar(pos, sub_df['host_ptw_latency'], width,
+#                bottom=sub_df['guest_ptw_latency'], label='host_ptw_latency')
+#     else:
+#         ax.bar(pos, sub_df['guest_ptw_latency'], width)
+#         ax.bar(pos, sub_df['host_ptw_latency'], width,
+#                bottom=sub_df['guest_ptw_latency'])
+
+plot_data.plot(kind='bar', stacked=True, ax=ax)
 
 # Setting custom labels and title
-ax.set_title(
-    'Guest and Host PTW Latency for different traces compared to baseline')
-ax.set_xticks(positions)
-ax.set_ylabel('Normalized PTW Latency')
-ax.set_xticklabels(labels, rotation=45, ha="right")
-ax.legend()
+# ax.set_title(
+#     'Guest and Host PTW Latency for different traces compared to baseline')
+# labels = [item.get_text() for item in ax.get_xticklabels()]
+# new_labels = [label.split(",") for label in labels]
+# print(new_labels)
+# ax.set_xticklabels([f"{label[0]}{label[1]}" for label in new_labels])
+# Extract unique workloads and configurations
+workloads = plot_data.index.get_level_values('Trace').unique()
+configs = plot_data.index.get_level_values('Exp').unique()
+
+ax.set_xticks(
+    [i + len(configs)/2 - 0.5 for i in range(0, len(plot_data), len(configs))])
+ax.set_xticklabels([], rotation=0, ha='center')  # Clear the default labels
+
+# Manually set the position of the configuration labels just above the bars
+for i, (workload, config) in enumerate(plot_data.index):
+    ax.text(i, -0.01, config, ha='center', va='top', rotation=90,
+            color='black', fontsize=10, transform=ax.get_xaxis_transform())
+
+# Manually set the position of the workload labels centered at the bottom of its corresponding configurations
+for i, workload in enumerate(workloads):
+    position = i * len(configs) + len(configs)/2 - 0.5
+    ax.text(position, -0.2, workload, ha='center', va='top', rotation=0,
+            color='black', fontsize=12, transform=ax.get_xaxis_transform())
+new_legend_labels = ['Guest', 'Host']
+
+ax.set_xlabel('')
+ax.legend(new_legend_labels)
 
 
 plt.tight_layout()
@@ -702,6 +768,7 @@ tlb_blocks["GMEAN"] = np.prod(
 print(tlb_blocks)
 x_ticks = list(tlb_blocks.keys())
 x_ticks = [x.replace("victima_ptw_2MBL2_", "") for x in x_ticks]
+
 
 # plot tlb blocks
 plt.figure(figsize=(12, 5))
--- scripts/parse_nn_results.py
+++ scripts/parse_nn_results.py
@@ -1,21 +1,27 @@
 import re
 import sys
 
+layers = [4, 4, 6, "N/A"]
+hidden_layers = [16, 64, 4, "N/A"]
+size = [6024, 70152, 776, 24]
 with open(sys.argv[1], "r") as f:
     lines = f.readlines()
     csv_data = [
-        "Model,Dim of Input,Dim of Output,Recall,Accuracy,Precision,F1-Score"]
+        "Model,FeatureSize,HiddenLayers,Size(B),Recall,Accuracy,Precision,F1-Score"]
 
     for i in range(0, len(lines), 6):
         model_line = lines[i]
-        dim_input = re.search(r"dim of input: (\d+)", model_line).group(1)
-        dim_output = re.search(r"dim of output: (\d+)", model_line).group(1)
-        recall = lines[i+1].split(": ")[1]
-        accuracy = lines[i+2].split(": ")[1]
-        precision = lines[i+3].split(": ")[1]
-        f1_score = lines[i+4].split(": ")[1]
+        layers_num = layers[i//6]
+        hidden = hidden_layers[i//6]
+        modelsize = size[i//6]
+        features = re.findall(r"\d+", model_line)[0]
+        recall = lines[i+1].split(": ")[1].strip("\n")
+        accuracy = lines[i+2].split(": ")[1].strip("\n")
+        precision = lines[i+3].split(": ")[1].strip("\n")
+        f1_score = lines[i+4].split(": ")[1].strip("\n")
+
         csv_data.append(
-            f"model,{dim_input},{dim_output},{recall},{accuracy},{precision},{f1_score}")
+            f"model,{features},{layers_num},{hidden},{modelsize},{recall},{accuracy},{precision},{f1_score}")
 
     with open("./plots/table2.csv", "w") as f:
         f.write("\n".join(csv_data))
